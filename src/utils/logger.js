19e4e1e2e882408e14fbb50d40935bc884f7d819
archae0pteryx <thefakeyran@icloud.com>
2020-01-14 11:50:37 -0800

    fix typo

--- src/utils/logger.js
+++ src/utils/logger.js
@@ -1,7 +1,7 @@
 const { NODE_ENV } = process.env
 let logger = {
   log: (msg) => console.log(msg),
-  error: (msg) = console.error(msg)
+  error: (msg) => console.error(msg)
 }
 
 if (NODE_ENV === 'production' || NODE_ENV === 'test') {
